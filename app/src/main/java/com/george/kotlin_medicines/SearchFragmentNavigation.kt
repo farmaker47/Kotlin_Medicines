4a5070ec8315fdd46217922b72cae2903eebe91a
farmaker47 <farmaker47@gmail.com>
2020-03-15 09:16:31 +0200

    Loads WebView content with data binding

--- app/src/main/java/com/george/kotlin_medicines/SearchFragmentNavigation.kt
+++ app/src/main/java/com/george/kotlin_medicines/SearchFragmentNavigation.kt
@@ -1,30 +1,77 @@
 package com.george.kotlin_medicines
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import androidx.navigation.fragment.findNavController
+import android.webkit.WebView
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.george.kotlin_medicines.databinding.FragmentSearchFragmentNavigationBinding
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
 
 /**
- * A simple [Fragment] subclass as the default destination in the navigation.
+ * A simple [Fragment] subclass.
+ * Use the [SearchFragmentNavigation.newInstance] factory method to
+ * create an instance of this fragment.
  */
 class SearchFragmentNavigation : Fragment() {
+    private var param1: String? = null
+    private var param2: String? = null
 
-    override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false)
+    private lateinit var webView : WebView
+    private lateinit var binding: FragmentSearchFragmentNavigationBinding
+    val URL_TO_SERVE = "https://services.eof.gr/drugsearch/SearchName.iface"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        view.findViewById<Button>(R.id.button_first).setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
-        }
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_search_fragment_navigation,container,false)
+        webView = binding.webViewEof
+
+        //Enable Javascript
+
+        //Enable Javascript
+        webView.settings.javaScriptEnabled = true
+        //Clear All and load url
+        webView.loadUrl(URL_TO_SERVE)
+
+
+
+        return binding.root
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment SearchFragmentNavigation.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            SearchFragmentNavigation().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
     }
 }
